a0c903768d908c25d349ee83f569f1d67ae6956e
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-03-19 14:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let first=document.getElementById('first');
+        let last=document.getElementById('last');
+        let phone=document.getElementById('phone');
+        let email=document.getElementById('email');
+        let submit=document.getElementById('submit');
+
+        submit.addEventListener('click',()=>{
+            // e.preventDefault();
+            alert(`
+            First Name: ${first.value}
+            Last Name: ${last.value}
+            Phone Number: ${phone.value}
+            Email: ${email.value}`);
+        })
